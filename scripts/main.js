03dbebb5a16ad1d3715ce64e53bac63fb1a93d11
Levi Hicks <lv.hicks@gmail.com>
2018-10-08 22:04:42 -0500

    add initial search functionality
    search button removed and replaced with results that appear as user types query

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,30 @@
-
 var $searchInput = $("#searchInput");
 var $searchButton = $("button[name=searchButton");
-var $searchList = $(".searchResults ol");
-var markets;
-$searchButton.on("click", function (event) {
-	var url = 'https://cors.io/?https://www.bittrex.com/api/v1.1/public/getmarkets';
-	fetch(url).then(function(response) {
-	  response.text().then(function(text) {
-	    markets = JSON.parse(text);
-	    displayResults();
-	  });
-	});
+var $searchList = $(".searchResults ul");
+var markets, query;
+var marketsURL = 'https://cors.io/?https://www.bittrex.com/api/v1.1/public/getmarkets';
+
+
+$searchInput.keyup(function(event){
+	query = event.target.value;
+	$searchList.empty();
+	if(markets){
+		markets.forEach(function(market){
+			if ((market.MarketCurrencyLong.toLowerCase().startsWith(query.toLowerCase())
+				|| market.MarketCurrency.toLowerCase().startsWith(query.toLowerCase()))
+				&& query!="")
+				$searchList.append($("<li>").text(market.MarketCurrencyLong));
+		});
+	}
 });
 
-function displayResults(){
-	console.log(markets.result[0].MarketCurrencyLong);
-}
+var main = function(){
+	fetch(marketsURL).then(function(response) {
+	  	response.text().then(function(text) {
+	    markets = JSON.parse(text);
+	    markets = markets.result;
+	  });
+	});
+};
+
+$(document).ready(main);
